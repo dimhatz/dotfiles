92aeea2d19345ea9dc8bbc448aed1136d8ef99f2
dimxdim <dimxdim@users.noreply.github.com>
2024-04-14 19:26:44 +0300

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -281,11 +281,11 @@ require('lazy').setup({
     'lewis6991/gitsigns.nvim',
     opts = {
       signs = {
-        add = { text = '│' },
-        change = { text = '│' },
-        delete = { text = '│' },
-        topdelete = { text = '│' },
-        changedelete = { text = '│' },
+        add = { text = '▒' },
+        change = { text = '▒' },
+        delete = { text = '▒' },
+        topdelete = { text = '▒' },
+        changedelete = { text = '▒' },
       },
     },
   },
