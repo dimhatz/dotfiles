4966b044e408d454a003b444a6e5f64513fe88ed
dimxdim <dimxdim@users.noreply.github.com>
2024-04-14 18:58:43 +0300

    setup surround

--- nvim/init.lua
+++ nvim/init.lua
@@ -99,6 +99,7 @@ remap('n', 'z', '"_d', { desc = 'delete into black hole' })
 remap('n', 'Z', '"_D', { desc = 'delete into black hole' })
 remap('n', 'X', '<cmd>echo "use Z to delete into black hole till end of line"<CR>')
 remap('n', 'zz', '"_dd', { desc = 'delete line into black hole' })
+remap('n', 'zw', '"_daw') -- be consistent with dw -> daw
 remap('v', 'x', '"_d')
 remap('v', 'z', '"_d')
 
@@ -107,9 +108,8 @@ remap('n', 'k', 'gk')
 remap('n', '{', '<Cmd>bprev<CR>', { silent = true })
 remap('n', '}', '<Cmd>bnext<CR>', { silent = true })
 
-remap('n', 'v', '<C-v>')
 remap('n', '<C-v>', 'V')
-remap('n', 'V', 'v')
+remap('n', 'V', '<C-v>')
 
 remap('n', '<C-\\>', 'gUiw') -- Uppercase word in norm/insert
 remap('i', '<C-\\>', '<Esc>gUiwea') -- FIXME: does not repeat
@@ -129,12 +129,12 @@ remap('n', 'mm', 'za', { desc = 'toggle fold (za)' })
 remap('v', 'p', 'p:let @v=@+|let @+=@0<CR>', { desc = 'Pasting in visual stores the overwritten text in "v register', silent = true })
 -- remap('v', 'p', 'p:let @v=@+<Bar>let @+=@0<CR>', { desc = 'Pasting in visual stores the overwritten text in "v register', silent = true }) -- also works
 
-remap('n', '<C-j>', '<C-e>', { desc = 'Scroll down 1 line' })
-remap('n', '<C-k>', '<C-y>', { desc = 'Scroll up 1 line' })
+remap('n', '<C-h>', '<C-e>', { desc = 'Scroll down 1 line' })
+remap('n', '<C-l>', '<C-y>', { desc = 'Scroll up 1 line' })
 remap('i', '<C-j>', '<C-o><C-e>', { desc = 'Scroll down 1 line' })
 remap('i', '<C-k>', '<C-o><C-y>', { desc = 'Scroll up 1 line' })
-remap('n', '<C-h>', '1<C-d>', { desc = 'Scroll down 1 line with cursor steady' })
-remap('n', '<C-l>', '1<C-u>', { desc = 'Scroll up 1 line with cursor steady' })
+remap('n', '<C-j>', '1<C-d>', { desc = 'Scroll down 1 line with cursor steady' })
+remap('n', '<C-k>', '1<C-u>', { desc = 'Scroll up 1 line with cursor steady' })
 -- remap('n', '<A-j>', '1<C-d>', { desc = 'Scroll down 1 line with cursor steady' }) -- same as above, but with Alt, alacritty re-exposes (in flashes) the mouse if its inside terminal
 -- remap('n', '<A-k>', '1<C-u>', { desc = 'Scroll up 1 line with cursor steady' })
 remap('n', '<C-m>', 'M', { desc = 'Put cursor in the center of the screen' })
@@ -154,6 +154,9 @@ remap('n', '<C-F10>', '<Cmd>set foldlevel=999<CR>', { desc = 'Unfold all' })
 -- now C-p and C-n autocomplete the beginning of the command and search.
 remap('c', '<C-p>', '<Up>', { desc = 'Autocomplete in command mode' })
 remap('c', '<C-n>', '<Down>', { desc = 'Autocomplete in command mode' })
+
+remap('n', 'cw', 'ciw')
+remap('n', 'dw', 'daw')
 
 -- -- Diagnostic keymps
 -- remap('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
@@ -569,111 +572,6 @@ require('lazy').setup({
     end,
   },
 
-  -- { -- Autocompletion
-  --   'hrsh7th/nvim-cmp',
-  --   event = 'InsertEnter',
-  --   dependencies = {
-  --     -- Snippet Engine & its associated nvim-cmp source
-  --     {
-  --       'L3MON4D3/LuaSnip',
-  --       build = (function()
-  --         -- Build Step is needed for regex support in snippets.
-  --         -- This step is not supported in many windows environments.
-  --         -- Remove the below condition to re-enable on windows.
-  --         if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-  --           return
-  --         end
-  --         return 'make install_jsregexp'
-  --       end)(),
-  --       dependencies = {
-  --         -- `friendly-snippets` contains a variety of premade snippets.
-  --         --    See the README about individual language/framework/plugin snippets:
-  --         --    https://github.com/rafamadriz/friendly-snippets
-  --         -- {
-  --         --   'rafamadriz/friendly-snippets',
-  --         --   config = function()
-  --         --     require('luasnip.loaders.from_vscode').lazy_load()
-  --         --   end,
-  --         -- },
-  --       },
-  --     },
-  --     'saadparwaiz1/cmp_luasnip',
-  --
-  --     -- Adds other completion capabilities.
-  --     --  nvim-cmp does not ship with all sources by default. They are split
-  --     --  into multiple repos for maintenance purposes.
-  --     'hrsh7th/cmp-nvim-lsp',
-  --     'hrsh7th/cmp-path',
-  --   },
-  --   config = function()
-  --     -- See `:help cmp`
-  --     local cmp = require 'cmp'
-  --     local luasnip = require 'luasnip'
-  --     luasnip.config.setup {}
-  --
-  --     cmp.setup {
-  --       snippet = {
-  --         expand = function(args)
-  --           luasnip.lsp_expand(args.body)
-  --         end,
-  --       },
-  --       completion = { completeopt = 'menu,menuone,noinsert' },
-  --
-  --       -- For an understanding of why these mappings were
-  --       -- chosen, you will need to read `:help ins-completion`
-  --       --
-  --       -- No, but seriously. Please read `:help ins-completion`, it is really good!
-  --       mapping = cmp.mapping.preset.insert {
-  --         -- Select the [n]ext item
-  --         ['<C-n>'] = cmp.mapping.select_next_item(),
-  --         -- Select the [p]revious item
-  --         ['<C-p>'] = cmp.mapping.select_prev_item(),
-  --
-  --         -- Scroll the documentation window [b]ack / [f]orward
-  --         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-  --         ['<C-f>'] = cmp.mapping.scroll_docs(4),
-  --
-  --         -- Accept ([y]es) the completion.
-  --         --  This will auto-import if your LSP supports it.
-  --         --  This will expand snippets if the LSP sent a snippet.
-  --         ['<C-y>'] = cmp.mapping.confirm { select = true },
-  --
-  --         -- Manually trigger a completion from nvim-cmp.
-  --         --  Generally you don't need this, because nvim-cmp will display
-  --         --  completions whenever it has completion options available.
-  --         ['<C-Space>'] = cmp.mapping.complete {},
-  --
-  --         -- Think of <c-l> as moving to the right of your snippet expansion.
-  --         --  So if you have a snippet that's like:
-  --         --  function $name($args)
-  --         --    $body
-  --         --  end
-  --         --
-  --         -- <c-l> will move you to the right of each of the expansion locations.
-  --         -- <c-h> is similar, except moving you backwards.
-  --         ['<C-l>'] = cmp.mapping(function()
-  --           if luasnip.expand_or_locally_jumpable() then
-  --             luasnip.expand_or_jump()
-  --           end
-  --         end, { 'i', 's' }),
-  --         ['<C-h>'] = cmp.mapping(function()
-  --           if luasnip.locally_jumpable(-1) then
-  --             luasnip.jump(-1)
-  --           end
-  --         end, { 'i', 's' }),
-  --
-  --         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-  --         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
-  --       },
-  --       sources = {
-  --         { name = 'nvim_lsp' },
-  --         { name = 'luasnip' },
-  --         { name = 'path' },
-  --       },
-  --     }
-  --   end,
-  -- },
-
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
@@ -681,37 +579,35 @@ require('lazy').setup({
     'echasnovski/mini.nvim',
     config = function()
       -- Better Around/Inside textobjects
-      --
-      -- Examples:
-      --  - va)  - [V]isually select [A]round [)]paren
-      --  - yinq - [Y]ank [I]nside [N]ext [']quote
-      --  - ci'  - [C]hange [I]nside [']quote
-      require('mini.ai').setup({ n_lines = 500 })
+      -- Auto-jumps to next text object: to jump+visual inside next parens: vi)
+      -- For larger scrope, press i) again
+      require('mini.ai').setup({ n_lines = 500 }) -- 50 default, 500 suggested by kickstart
 
-      -- Add/delete/replace surroundings (brackets, quotes, etc.)
-      --
-      -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-      -- - sd'   - [S]urround [D]elete [']quotes
-      -- - sr)'  - [S]urround [R]eplace [)] [']
-      require('mini.surround').setup()
+      require('mini.surround').setup({
+        mappings = {
+          add = 's', -- Add surrounding in Normal and Visual modes
+          delete = 'sd', -- Delete surrounding
+          find = 'sf', -- Find surrounding (to the right)
+          find_left = 'sF', -- Find surrounding (to the left)
+          highlight = 'sh', -- Highlight surrounding
+          replace = 'sc', -- Replace surrounding
+          update_n_lines = 'su', -- Update `n_lines` (how many lines are searched to perform surround actions)
+          suffix_last = 'l', -- Suffix to search with "prev" method
+          suffix_next = 'n', -- Suffix to search with "next" method
+        },
+      })
 
-      -- Simple and easy statusline.
-      --  You could remove this setup call if you don't like it,
-      --  and try some other statusline plugin
+      remap('n', 'sw', 'siw', { remap = true }) -- be consistent with cw -> ciw
+      remap('n', 'ds', 'sd', { remap = true })
+      remap('n', 'cs', 'sc', { remap = true })
+
       local statusline = require('mini.statusline')
-      -- set use_icons to true if you have a Nerd Font
       statusline.setup({ use_icons = vim.g.have_nerd_font })
 
-      -- You can configure sections in the statusline by overriding their
-      -- default behavior. For example, here we set the section for
-      -- cursor location to LINE:COLUMN
       ---@diagnostic disable-next-line: duplicate-set-field
       statusline.section_location = function()
         return '%2l:%-2v'
       end
-
-      -- ... and there is more!
-      --  Check out: https://github.com/echasnovski/mini.nvim
     end,
   },
 
