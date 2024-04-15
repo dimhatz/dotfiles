3411061ebf304cb53c4613d81869dc1e0c1d3475
dimxdim <dimxdim@users.noreply.github.com>
2024-04-15 20:43:49 +0300

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -245,6 +245,10 @@ require('lazy').setup({
   {
     'lunarvim/darkplus.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
+    init = function()
+      -- vim.cmd.colorscheme('darkplus')
+      -- vim.cmd.hi 'Comment gui=none'
+    end,
   },
   {
     'folke/tokyonight.nvim',
