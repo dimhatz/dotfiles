91c90229067e082f7b86f39b11071e003ec34d66
dimxdim <dimxdim@users.noreply.github.com>
2024-07-24 21:52:13 +0300

    init nix config

---
+++ nix/flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "Test flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOs/nixpkgs/nixos-24.05";
+  };
+
+  outputs = { self, nixpkgs, ...}:
+    let
+      lib = nixpkgs.lib;
+    in  {
+    nixosConfigurations = {
+      mypc = lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ ./configuration.nix ];
+      };
+    };
+  };
+}
