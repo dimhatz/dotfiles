3d86673f9c18c6954222d14a2b43ca385b0f9f4a
dimxdim <dimxdim@users.noreply.github.com>
2025-01-30 17:06:50 +0200

    better visual repeat intermediate 1

---
+++ nvim/lua/better-visual-repeat.lua
@@ -0,0 +1,377 @@
+-- TODO: maybe not even map v,V but use mode changes to start recording?
+-- TODO: expose a function is_active -> `not tracking == nothing` to be used in statusline
+-- TODO: maybe just diff 20 above/below lines to get inserted text? Problematic when pasting in visual.
+local M = {}
+local opt = {
+  ---We need to be able to tell whether it was e.g. 'c' or 'gc' (or some user mapping) that
+  ---triggered change to be able to replay just the change on visual-selection-dot
+  -- TODO: search for `v_` in telescope for all possible default shortcuts?
+  -- mappings_that_edit_in_visual = { 'm', 'x', 'X', 'd', 'D', 'c', 'p', 'r', 'gc', 's', '>', '<', 'U' },
+  -- TODO: support non-same-string sequences, to be able to say: {'a', 'b'} triggers change, but the rest
+  -- of mappings beginning with 'a' do not.
+  mappings_that_edit_in_visual = { 's' }, -- TODO: check 'r'
+  ---This would be likely due to user incorrectly applying workaround with force_alive
+  warn_on_keystrokes_recorded = 500,
+}
+
+local enable_logging = true -- for debugging
+
+local force_alive = false
+-- local REG1 = 'z'
+-- local REG2 = 'x'
+local changed_tick_at_start = -1
+local is_active = false
+---do not reset in reset_state(), used only in Better_visual_repeat() and mappings to
+---avoid triggering recording when replaying with non-! ":normal "
+-- must store visual_mode, we cannot rely on vim.fn.visualmode()
+-- since there could be a visual block with no change, before our (valid) repeat is called.
+local visual_mode_at_start = 'v' ---@type 'v' | 'V'
+local keys_recorded = {} ---@type string[]
+local insert_was_entered = false
+local keys_len_at_first_change = -1 --- -1 for "there was no change yet"
+---@type { keys: string[], keys_len_at_first_change: integer, mode: 'v' | 'V', dot_register_info: table?  }
+local last_valid_edit = {
+  keys = {}, --- will be used as part of a macro string
+  keys_len_at_first_change = -1, --- index of last element of keys, after the first text change
+  mode = 'v',
+  ---nil means non-insert-change (like, gc or surround), in this case
+  ---we will not need to append <Esc> to macro string before replaying.
+  dot_register_info = nil,
+}
+
+function M.setup(user_options)
+  -- vim.tbl_deep_extend
+end
+
+---@param must_be_true boolean
+---@param message string
+local function assert(must_be_true, message)
+  if must_be_true ~= true then
+    message = message .. 'Better visual repeat: '
+    vim.notify(message, vim.log.levels.ERROR)
+  end
+end
+
+local log = function(...)
+  if not enable_logging then
+    return
+  end
+  vim.cmd.redraw() -- to avoid 'press enter' in :messages
+  vim.print(...)
+end
+
+function M.toggle_logging()
+  enable_logging = not enable_logging
+end
+
+-- is vim macro active
+-- cursormovedI autocmd
+local function is_vim_macro_active()
+  return vim.fn.reg_recording() ~= '' or vim.fn.reg_executing() ~= ''
+end
+
+local function reset_state()
+  log('Resetting state') -- but not overwriting last valid edit
+  force_alive = false
+  changed_tick_at_start = -1
+  is_active = false
+  visual_mode_at_start = 'v'
+  keys_recorded = {}
+  insert_was_entered = false
+end
+
+Better_visual_noop = function() end
+
+-- @param motion: when called the first time by us, will be nil.
+-- When called as a repeat (g@l) by nvim, it will be set to 'char' (see :h g@)
+-- must be global in order to be able to use operatorfunc
+function Better_visual_repeat(motion)
+  if motion == nil then
+    log('First call')
+    changed_tick_at_start = vim.api.nvim_buf_get_var(0, 'changedtick')
+    return
+  end
+
+  -- this is repeat by .
+  local t_begin = os.clock()
+  log('Repeat call')
+  log(last_valid_edit)
+
+  M.stop('Repeat call')
+
+  -- NOTE: in ". register, when typing <, it is captured as a regular 1-char '<', not as 4-char '<lt>'
+  -- string, so no need to additionally escape.
+  -- NOTE: dot register needs to be executed with bang!, noremapped aka normal! otherwise
+  -- chars like +U will be added, hence the 'n' flag.
+  -- The recorded movements including the final edit command should be executed as mapped, non-bang,
+  -- to replicate any potential remaps, hence the 'm' flag.
+
+  -- 'n' to avoid triggering our mapping that will start recording again
+  vim.api.nvim_feedkeys(last_valid_edit.mode, 'n', false)
+  local movement_keys_with_edit = table.concat(last_valid_edit.keys)
+  vim.api.nvim_feedkeys(movement_keys_with_edit, 'm', false)
+
+  if last_valid_edit.dot_register_info then
+    -- TODO: just save the register as single string with getreg(). Are there cases
+    -- dot register may be linewise?
+    -- escape_ks = true does not seem to affect arrow movement
+    -- adding 'x' flag will make it exit insert, not adding <Esc> for this reason
+    vim.api.nvim_feedkeys(table.concat(last_valid_edit.dot_register_info.regcontents, '\n'), 'nx', false)
+  end
+  -- vim.api.nvim_feedkeys('', 'nx', false) -- executes typeahead, will exit insert
+
+  log((os.clock() - t_begin) * 1000 .. ' ms') -- ms, measured 0ms - 2ms
+
+  -- needed so that a second (consequtive) . repeats g@l instead of doing regular repeat
+  -- TODO: check if needed to wrap with vim.schedule(): record a visual
+  -- edit, do a non-visual one, do a dot-on-selection, do a . in normal. If what repeats
+  -- is not our function, but the previous (unrecorded non-visual) edit, wrapping may be needed.
+  vim.go.operatorfunc = 'v:lua.Better_visual_noop'
+  vim.cmd('normal! g@l')
+  vim.go.operatorfunc = 'v:lua.Better_visual_repeat'
+end
+
+---Can be called from a mapping as a workaround for another plugin.
+---Stop all recording.
+---@param abort_with_reason string?
+---If abort_with_reason is provided, we will abort and discard / invalidate anything that was recorded.
+---Otherwise we will follow the usual steps to determine whether the recorded edit is valid.
+function M.stop(abort_with_reason)
+  log('Stopping rec')
+
+  local changed_tick_current = vim.api.nvim_buf_get_var(0, 'changedtick')
+  if changed_tick_current == changed_tick_at_start then
+    -- no changes were made
+    abort_with_reason = 'No changes detected'
+  end
+
+  if abort_with_reason ~= nil then
+    log('Aborting: ' .. abort_with_reason)
+    reset_state()
+    return
+  end
+
+  -- at this point the recorded edit is valid and can be repeated.
+  -- If an edit was made in insert the text will be in ". register. If 'c' was pressed and
+  -- nothing was entered (text was deleted), ". register will be empty string ("")
+  log('Updating captured command')
+  last_valid_edit = {
+    keys = keys_recorded,
+    mode = visual_mode_at_start,
+    dot_register_info = insert_was_entered and vim.fn.getreginfo('.') or nil,
+    keys_len_at_first_change = keys_len_at_first_change,
+  }
+  reset_state()
+
+  vim.schedule(function()
+    -- schedule is needed, otherwise just the last action is repeated, not our g@l
+    log('setting last cmd to our g@l')
+    -- vim history has the individual edit as last, force it to be our g@l
+    vim.go.operatorfunc = 'v:lua.Better_visual_noop'
+    vim.cmd('normal! g@l')
+    vim.go.operatorfunc = 'v:lua.Better_visual_repeat'
+  end)
+end
+
+-------------------- On key / autocmds -----------------------------------------------
+
+local on_key_logging = false -- for debugging
+
+local ns = vim.api.nvim_create_namespace('BetterVisualRepeat')
+vim.on_key(function(mapped, typed)
+  -- This callback is triggered before ModeChanged's callback
+  if not is_active then
+    return
+  end
+
+  if on_key_logging then
+    -- TODO: for better logging see what which-key uses to print register content
+    -- it shows the "decoded" chars instead of byte-like sequences
+    log('mapped: ' .. mapped .. ' typed: ' .. typed .. ' ')
+  end
+  if insert_was_entered then
+    return
+  end
+  if typed ~= '' then
+    table.insert(keys_recorded, typed)
+  end
+end, ns)
+
+vim.api.nvim_create_autocmd({ 'RecordingEnter' }, {
+  desc = "Better visual repeat - disable on vim's recording",
+  group = vim.api.nvim_create_augroup('better-visual-recording-disable', { clear = true }),
+  callback = function()
+    if not is_active then
+      return
+    end
+    M.stop('Vim recording started')
+  end,
+})
+
+vim.api.nvim_create_autocmd({ 'ModeChanged' }, {
+  desc = 'Better visual repeat update',
+  group = vim.api.nvim_create_augroup('better-visual-repeat-modes', { clear = true }),
+  callback = function()
+    -- This callback is triggered after on_key's callback
+    -- Determines whether it's time to stop or should we go on.
+    if not is_active then
+      return
+    end
+
+    local old_mode = vim.v.event.old_mode
+    local new_mode = vim.v.event.new_mode
+    log(old_mode .. ' ' .. new_mode .. ' ' .. vim.api.nvim_buf_get_var(0, 'changedtick'))
+
+    if force_alive then
+      return
+    end
+
+    if new_mode == 'i' then
+      insert_was_entered = true
+      return
+    end
+
+    -- mode 'no' stands for normal operator pending, see :h mode()
+    if new_mode == 'v' or new_mode == 'V' or new_mode == 'no' or old_mode == 'no' then
+      -- do not cancel recording when switching between regular / line visual, or making changes
+      return
+    end
+
+    -- we are out of visual now, so stopping recording
+    M.stop()
+  end,
+})
+
+vim.api.nvim_create_autocmd({ 'TextChanged' }, {
+  desc = 'Better visual repeat text change',
+  group = vim.api.nvim_create_augroup('better-visual-repeat-text-change', { clear = true }),
+  callback = function()
+    if not is_active then
+      return
+    end
+    log('Text changed')
+    if keys_len_at_first_change ~= -1 then
+      log('Updating keys_len_at_first_change')
+      keys_len_at_first_change = #keys_recorded
+    end
+  end,
+})
+
+------------------- Mapping functions ---------------------------------------------
+
+function M.better_v()
+  vim.cmd('normal! v')
+  if is_vim_macro_active() then
+    log('Regular recording / replaying ongoing.')
+    return
+  end
+  reset_state()
+  log('start v ' .. vim.api.nvim_buf_get_var(0, 'changedtick'))
+  is_active = true
+  visual_mode_at_start = 'v'
+  Better_visual_repeat()
+end
+
+function M.better_V()
+  vim.cmd('normal! V')
+  if is_vim_macro_active() then
+    log('Regular recording / replaying ongoing.')
+    return
+  end
+  reset_state()
+  log('start V ' .. vim.api.nvim_buf_get_var(0, 'changedtick'))
+  is_active = true
+  visual_mode_at_start = 'V'
+  Better_visual_repeat()
+end
+
+function M.dot_on_visual_selection()
+  if is_vim_macro_active() then
+    log('Regular recording / replaying ongoing. Will not continue.')
+    M.stop('Vim recording ongoing')
+    return
+  end
+
+  assert(is_active, 'Dot on visual selection: expected is_active=true')
+
+  if #last_valid_edit.keys == 0 or not is_active then
+    log('No keys recorded yet') -- the is_active case is handled above
+    -- no need to M.stop() here, recording can continue
+    return
+  end
+
+  local mode = vim.fn.mode(1)
+  if mode ~= 'v' and mode ~= 'V' then
+    log('Not in visual, but in: ' .. mode)
+    M.stop('Unexpected mode: ' .. mode)
+    return
+  end
+
+  M.stop('Will do . on visual selection') -- not defensive, on every visual we always start recording
+
+  log(last_valid_edit)
+  local idx_recorded = last_valid_edit.keys_len_at_first_change
+  local idx_custom = -1
+  for i, key_str in ipairs(last_valid_edit.keys) do
+    if vim.tbl_contains(opt.mappings_that_edit_in_visual, key_str) then
+      idx_custom = i
+      break
+    end
+  end
+  local idx_final = -1
+  if idx_recorded == -1 then
+    idx_final = idx_custom
+  elseif idx_custom ~= -1 and idx_custom < idx_recorded then
+    idx_final = idx_custom
+  else
+    idx_final = idx_recorded
+  end
+
+  local edit_macro = ''
+  if idx_final == -1 then
+    edit_macro = table.concat(last_valid_edit.keys)
+  else
+    edit_macro = table.concat(last_valid_edit.keys, '', idx_final)
+  end
+
+  -- vim.fn.setreg(REG1, edit_macro)
+  --
+  -- vim.cmd('normal! @' .. REG1)
+
+  -- -- find whichever of input_chars_that_edit_in_visual is first, note that some of them might
+  -- -- be >1 chars
+  -- local edit_char_idx = 0 -- zero for 'not found'
+  --
+  -- for i = 1, #last_valid_macro do
+  --   local macro_substring = last_valid_macro:sub(i)
+  --   for _, mapping in ipairs(mappings_that_edit_in_visual) do
+  --     if vim.startswith(macro_substring, mapping) then
+  --       edit_char_idx = i
+  --       break
+  --     end
+  --   end
+  --   if edit_char_idx ~= 0 then
+  --     break
+  --   end
+  -- end
+  --
+  -- if edit_char_idx == 0 then
+  --   vim.notify('Visual repeat: could not find the mapping that edits in the recorded macro: ' .. last_valid_macro, vim.log.levels.ERROR)
+  --   return
+  -- end
+  --
+  -- local edit_part_of_macro = last_valid_macro:sub(edit_char_idx)
+  -- vim.fn.setreg(MY_MACRO_REGISTER2, edit_part_of_macro)
+  --
+  -- vim.cmd('normal! @' .. MY_MACRO_REGISTER2)
+end
+
+------------------- Test mappings ---------------------------------------------
+
+vim.keymap.set('n', '<C-m>', function()
+  on_key_logging = not on_key_logging
+end, { desc = 'Better visual repeat debug: Toggle on_key() logging' })
+
+vim.keymap.set('n', 'v', M.better_v, { desc = 'Better v' })
+vim.keymap.set('n', '<C-v>', M.better_V, { desc = 'Better V' })
+vim.keymap.set('v', '.', M.dot_on_visual_selection, { desc = 'Better . on visual selection' })
