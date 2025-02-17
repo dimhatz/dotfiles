d725ce18fc444235a5bdbefa99c60a83e9d959f5
dimhatz <7132507+dimhatz@users.noreply.github.com>
2025-02-17 23:12:35 +0200

    Update my-better-visual-repeat.lua

--- nvim/lua/my-better-visual-repeat.lua
+++ nvim/lua/my-better-visual-repeat.lua
@@ -13,7 +13,21 @@ return {
     bvr.patch_MatchitVisualForward()
     vim.keymap.set('n', 'v', bvr.better_v, { desc = 'Better v' })
     vim.keymap.set('n', '<C-v>', bvr.better_V, { desc = 'Better V' })
-    vim.keymap.set('v', '.', bvr.dot_on_visual_selection, { desc = 'Better . on visual selection' })
-    vim.keymap.set('v', '<C-v>', 'V', { desc = '<c-v> is new V in visual' })
+    vim.keymap.set('x', '.', bvr.dot_on_visual_selection, { desc = 'Better . on visual selection' })
+    vim.keymap.set('x', '<C-v>', 'V', { desc = '<c-v> is new V in visual' })
+    vim.keymap.set('n', 'V', '<C-v>', { desc = 'V is new <C-v>' })
+    vim.keymap.set('x', 'V', '<C-v>', { desc = 'V is new <C-v> in visual' })
+    vim.keymap.set('x', 'I', function()
+      if vim.fn.mode(1) == 'V' then
+        return '<C-v>0I'
+      end
+      return 'I'
+    end, { expr = true, desc = 'I now also works in visual line' })
+    vim.keymap.set('x', 'A', function()
+      if vim.fn.mode(1) == 'V' then
+        return '<C-v>$A'
+      end
+      return 'A'
+    end, { expr = true, desc = 'A now also works in visual line' })
   end,
 }
