3d80ceb5f10dbaead2613daba643e4f34e723df3
dimhatz <7132507+dimhatz@users.noreply.github.com>
2026-03-01 15:10:55 +0200

    update typescript-tools, plenary

--- nvim/lua/my-profile.lua
+++ nvim/lua/my-profile.lua
@@ -1,5 +1,6 @@
 local remap = require('my-helpers').remap
 
+-- NOTE: the settings are for plugin version commit 0ee32b7aba31d84b0ca76aaff2ffcb11f8f5449f
 -- feed the produced file to https://ui.perfetto.dev/
 return {
   'stevearc/profile.nvim',
