2997c1e1445e8754a7808fb6a61583003ea04b68
dimhatz <7132507+dimhatz@users.noreply.github.com>
2026-03-04 17:33:42 +0200

    update treesitter + rainbow delimiters

--- nvim/lua/my-treesitter.lua
+++ nvim/lua/my-treesitter.lua
@@ -1,10 +1,14 @@
 -- docs at :help nvim-treesitter
+-- We use treesitter only for rainbow delimiters(triggered on esc), the rest of highlighting is provided by syntax + lsp
 return {
   'nvim-treesitter/nvim-treesitter',
   enabled = true,
-  build = ':TSUpdate',
-  opts = {
-    ensure_installed = {
+  lazy = false,
+  branch = 'main',
+  -- build = ':TSUpdate',
+  config = function()
+    local ts = require('nvim-treesitter')
+    local langs = {
       'bash',
       'c',
       'lua',
@@ -27,24 +31,10 @@ return {
       -- 'javascriptreact',
       -- 'typescriptreact',
       -- 'sass',
-    },
-    -- Autoinstall languages that are not installed
-    auto_install = true,
-    highlight = {
-      enable = false,
-      -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-      --  If you are experiencing weird indenting issues, add the language to
-      --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-      additional_vim_regex_highlighting = { 'ruby' },
-    },
-    -- disable indent, since we use mini-indentscope, == operator is not that useful too, since
-    -- we use autoformatters anyway. rainbow-delimiters that requires treesitter seems to not be
-    -- affected by this, continues to work.
-    indent = { enable = false, disable = { 'ruby' } },
-    incremental_selection = { enable = false },
-  },
-  config = function(_, opts)
-    require('nvim-treesitter.configs').setup(opts)
+    }
+
+    -- async, no-op if the parsers are already installed
+    ts.install(langs, { max_jobs = 1 }) -- summary = true,
 
     -- disable lua treesitter
     vim.api.nvim_create_autocmd({ 'FileType' }, {
@@ -56,12 +46,5 @@ return {
         vim.treesitter.stop()
       end,
     })
-
-    -- There are additional nvim-treesitter modules that you can use to interact
-    -- with nvim-treesitter. You should go explore a few and see what interests you:
-    --
-    --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-    --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-    --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   end,
 }
